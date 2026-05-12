9c5d149ca8d35c34bd385c0de05f7aa230b1ddca
oscardavidgomezoyos2007 <oscardavidgomezoyos2007@gmail.com>
2026-05-12 11:48:42 -0500

    mejoras 2 y 3

--- funciones.js
+++ funciones.js
@@ -1,1 +1,27 @@
-//AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos,egresos) {
+    let disponible = ingresos-egresos;
+    if (disponible < 0) {
+        disponible = 0;
+    }
+     return disponible;
+    
+}
+
+function calcularCapacidadDePago(montoDisponible) {
+    let calcularCapacidadDePago=montoDisponible/2;
+
+    return calcularCapacidadDePago;
+}
+
+
+
+function mostrarEnSpain(idSpain,valor) {
+    let componente = document.getElementById(idSpain);
+    componente.textContent = valor;
+}
+
+function text (id,vari) {
+    let text1 = document.getElementById(id);
+    texto1.innerText=vari;
+    ("spnDisponible",disponible.toFixed(2))
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,17 @@
-//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+//AQUI EL JAVARCRIPT PARA MANIPULAR EL HTML
+    function calcular() {
+    
+    let ingresos = parseFloat (document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+
+     let disponible=calcularDisponible(ingresos,egresos);
+
+      Texto("spnDisponible,disponible.parseFloat")
+      mostrarEnSpain("spnDisponible",disponible.toFixed(2))
+
+     let calcularCapacidadDePago = calcularCapacidadDePago(disponible)
+    
+     text("spnCapacidadDePago", calcularCapacidadDePago)
+}
+
